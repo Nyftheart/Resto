656e7731b4e895ae743690f2f62745b5d646b006
Flavio <flaviocabecas2@gmail.com>
2020-09-28 10:06:06 +0200

    affichage commandes restaurant

--- commandes.php
+++ commandes.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<?php $pdo = new PDO("mysql:host=localhost;dbname=test", "root", "", array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+<?php $pdo = new PDO("mysql:host=localhost;dbname=restaurant", "root", "", array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 ?>
 
 <?php
